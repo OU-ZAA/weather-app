e6e90881bf1701f6d9b19125e34c6830cb85f40d
Mohamed ouzaa <ouzaa.contact@gmail.com>
2023-07-21 02:27:06 +0100

    Refactor some code and implement a new design

---
+++ src/components/SearchForm.tsx
@@ -0,0 +1,46 @@
+import { useForm, SubmitHandler } from "react-hook-form";
+import { HiOutlineSearch } from "react-icons/hi";
+import axios from "axios";
+import { Coordinates } from "@/pages";
+import { Dispatch, SetStateAction } from "react";
+
+interface FormValues {
+  cityName: string;
+}
+
+async function getLocationByCity(cityName: string) {
+  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
+  const baseUrl = "http://api.openweathermap.org/geo/1.0/direct";
+
+  const { data } = await axios.get(
+    `${baseUrl}?q=%${cityName}&limit=1&appid=${apiKey}`
+  );
+  return data as Coordinates[];
+}
+
+export default function SearchForm({
+  setCoordinates,
+}: {
+  setCoordinates: Dispatch<SetStateAction<Coordinates | undefined>>;
+}) {
+  const { register, handleSubmit } = useForm<FormValues>();
+  const onSubmit: SubmitHandler<FormValues> = async (values) => {
+    const data = await getLocationByCity(values.cityName);
+    setCoordinates(data[0]);
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit(onSubmit)}
+      className="bg-white flex items-center gap-4 rounded-lg py-2 px-4 w-96"
+    >
+      <HiOutlineSearch className="w-6 h-6" />
+      <input
+        type="text"
+        {...register("cityName")}
+        className="appearance-none bg-transparent border-none w-full text-gray-700 leading-tight focus:outline-none"
+        placeholder="Los Angeles, CA"
+      />
+    </form>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,6 @@
-import HourlyForecast from "@/components/HourlyForecast";
+import SearchForm from "@/components/SearchForm";
 import TodayWeather from "@/components/TodayWeather";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import { useForm, SubmitHandler } from "react-hook-form";
-import { HiOutlineSearch } from "react-icons/hi";
-import { MdLocationOn } from "react-icons/md";
-import { useGeolocated } from "react-geolocated";
-
-interface FormValues {
-  cityName: string;
-}
+import { useState } from "react";
 
 export interface Coordinates {
   name?: string;
@@ -20,81 +11,15 @@ export interface Coordinates {
   local_names?: Record<string, string>;
 }
 
-async function getLocation(cityName: string) {
-  const apiKey = process.env.NEXT_PUBLIC_API_KEY;
-  const baseUrl = "http://api.openweathermap.org/geo/1.0/direct";
-
-  const { data } = await axios.get(
-    `${baseUrl}?q=%${cityName}&limit=1&appid=${apiKey}`
-  );
-  return data as Coordinates[];
-}
-
 export default function Home() {
   const [coordinates, setCoordinates] = useState<Coordinates>();
-  console.log("coordinates:", coordinates);
-
-  const { coords } = useGeolocated({
-    positionOptions: {
-      enableHighAccuracy: false,
-    },
-    isOptimisticGeolocationEnabled: true,
-  });
-
-  useEffect(() => {
-    async function getLocation() {
-      const { data } = await axios.get(
-        `http://api.openweathermap.org/geo/1.0/reverse?lat=${coords?.latitude}&lon=${coords?.longitude}&appid=${process.env.NEXT_PUBLIC_API_KEY}`
-      );
-      setCoordinates(data[0]);
-    }
-    getLocation();
-  }, []);
-
-  const { register, handleSubmit } = useForm<FormValues>();
-  const onSubmit: SubmitHandler<FormValues> = async (values) => {
-    const data = await getLocation(values.cityName);
-    setCoordinates(data[0]);
-  };
 
   return (
-    <div className="flex h-screen">
-      <aside className="px-12 py-8 w-[467px] bg-blue-800">
-        <div className="flex flex-col justify-between h-full">
-          <div className="flex flex-col gap-8">
-            <form
-              onSubmit={handleSubmit(onSubmit)}
-              className="w-full bg-white flex items-center gap-4 rounded-lg py-2 px-4"
-            >
-              <HiOutlineSearch className="w-6 h-6" />
-              <input
-                type="text"
-                {...register("cityName")}
-                className="appearance-none bg-transparent border-none w-full text-gray-700 leading-tight focus:outline-none"
-                placeholder="Los Angeles, CA"
-              />
-            </form>
-            <div className="h-48 bg-white rounded-lg"></div>
-            <div className="text-white">
-              <h3 className="text-7xl">23 C</h3>
-              <p className="text-3xl">Mostly Cloudy</p>
-              <p className="text-2xl">Friday, 11:54</p>
-            </div>
-          </div>
-          <div className="text-white flex items-center gap-4">
-            <MdLocationOn className="w-6 h-6" />
-            <p className="text-2xl">Los angeles, CA</p>
-          </div>
-        </div>
-      </aside>
-      {coordinates ? (
-        <>
-          <TodayWeather {...coordinates} />
-          <HourlyForecast {...coordinates} />
-        </>
-      ) : (
-        <div>Enter a city</div>
-      )}
-    </div>
+    <main className="h-screen bg-[url(/images/clouds.jpg)] bg-cover px-12 py-8">
+      <nav className="flex justify-center items-center">
+        <SearchForm setCoordinates={setCoordinates} />
+      </nav>
+      {coordinates && <TodayWeather {...coordinates} />}
+    </main>
   );
 }
